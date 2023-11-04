98386b81ecce57054ab8f79aba91825ebdbf8492
Mostafaa-Elkhouly <ms.elkhouly97@gmail.com>
2023-11-04 14:16:28 +0200

    Update GCP provider - Remote S3 backend

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -4,29 +4,22 @@ provider "google" {
 }
 
 variable "gcp_service_list" {
-  description ="The list of apis necessary for the project"
-  type = list(string)
+  description = "The list of APIs necessary for the project"
+  type        = list(string)
   default = [
-    "cloudresourcemanager.googleapis.com",
-    "serviceusage.googleapis.com",
-    "logging.googleapis.com",
-    "monitoring.googleapis.com",
     "compute.googleapis.com",
     "container.googleapis.com",
-    "artifactregistry.googleapis.com",
-    "iam.googleapis.com"
+    "artifactregistry.googleapis.com"
   ]
 }
 
-resource "google_project_service" "gcp_services" {
-  for_each = toset(var.gcp_service_list)
-  project = var.project_id
-  service = each.key
+# resource "google_project_service" "gcp_services" {
+#   count   = length(var.gcp_service_list)
+#   project = var.project_id
+#   service = var.gcp_service_list[count.index]
 
-  timeouts {
-    create = "30m"
-    update = "40m"
-  }
-
-  disable_dependent_services = true
-}
+#   timeouts {
+#     create = "30m"
+#     update = "40m"
+#   }
+# }
--- Terraform/remote-backend.tf
+++ Terraform/remote-backend.tf
@@ -1,4 +1,11 @@
+data "google_storage_bucket" "existing_bucket" {
+  name = "terraform-backend-tfstate-bucket"
+}
+
 resource "google_storage_bucket" "bucket" {
+  # Create only if the bucket doesn't exist
+  count = length(data.google_storage_bucket.existing_bucket) == 0 ? 1 : 0  
+
   name = "terraform-backend-tfstate-bucket"
   #location      = "us-central1" -> regional
   location      = "US" #multi region in US
